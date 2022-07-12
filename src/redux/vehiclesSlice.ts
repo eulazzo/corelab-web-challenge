e31e71e6ad7ae5931d568f982932cf9ec0b46d95
Lázaro Vanderson <eulazzo@protonmail.com>
2022-07-11 21:03:44 -0300

    initial commit

---
+++ src/redux/vehiclesSlice.ts
@@ -0,0 +1,57 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IVehicle } from "../types/Vehicle";
+
+
+type Vehicle= {
+  id: number,
+  name: string,
+  description: string,
+  plate:string,
+  is_favorite: boolean,
+  year: number,
+  color: string,
+  price: number,
+  created_at?:Date,
+  updated_at?: Date,
+}
+
+type InitialState = {
+  vehicles:IVehicle[],
+};
+
+const initialState:InitialState = {
+  vehicles:[]
+}
+
+const vehicleSlice = createSlice({
+  name: "vehicle",
+  initialState,
+
+  reducers: {
+    addVehicle: (state, action:PayloadAction<IVehicle[]>) => {
+      state.vehicles.push(...action.payload);
+    },
+    getVehiclesStore: (state) => {
+      return state.vehicles.forEach(car=> [{...car}])
+    },
+    getVehicleByIdStore: (state, action:PayloadAction<number>) => {
+     state.vehicles = state.vehicles.filter(vehicle => vehicle.id === action.payload)
+    },
+    deleteVehicleById: (state, action:PayloadAction<number>) => {
+      state.vehicles = state.vehicles.filter(vehicle => vehicle.id !== action.payload)
+    },
+    editVehicleById: (state, action:PayloadAction<IVehicle[]|any>) => {
+      return state.vehicles.forEach((vehicle) => {
+        if (vehicle.id === action.payload.id) {
+           return [{...vehicle}, action.payload];
+        } else {
+          return vehicle;
+        }
+      }); 
+       
+    },
+  },
+});
+
+export const { addVehicle, editVehicleById,deleteVehicleById,getVehicleByIdStore,getVehiclesStore} = vehicleSlice.actions;
+export default vehicleSlice.reducer;
